9868cd29f34c7908999fa7ba3ce2e1c3ca52a538
Jackson Coleman <wcoleman@colgate.edu>
2022-11-04 14:39:18 -0500

    Fixed navbar wrapping

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,15 @@
 import { NavLink } from "react-router-dom"
+import navCSS from "../styles/Navbar.css"
 
-export default function Navbar ({user}) {
+export default function Navbar (){
     return(
-        <div className="navbar">
+        <nav className="navbar">
             <NavLink className="main" to="/">Jackson Coleman</NavLink>
             <NavLink to="/projects">Projects</NavLink>
             <NavLink to="/experience">Experience</NavLink>
             <NavLink to="/education">Education</NavLink>
             <NavLink to="/skills">Skills</NavLink>
             <NavLink to="/writing">Writing</NavLink>
-        </div>
+        </nav>
     )
 }

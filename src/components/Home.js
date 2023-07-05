e0ce9732f7967092be359b78e7b680583aedd330
JacksonColeman <107893014+JacksonColeman@users.noreply.github.com>
2023-07-05 14:48:51 -0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,7 @@ export default function Home(){
     return(
     <div className="Home">
         <h1 className="header">Jackson Coleman</h1>
-        <h2>Full-Stack Software Engineer</h2>
+        {/*<h2>Full-Stack Software Engineer</h2> */}
         {/* <img src={mcC} alt="me at the art institute" height="250px"/>
         <img src={fishingPic} alt="me at the art institute" height="250px"/>
         <img src={snowy} alt="me at the art institute" height="250px"/> */}
